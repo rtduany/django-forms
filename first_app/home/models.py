49bd248058f77a91fac97ba8a1aeea80adf0ad87
rtduany <rtduany@gmail.com>
2015-10-09 01:44:27 +0300

    added classroom model

--- first_app/home/models.py
+++ first_app/home/models.py
@@ -4,9 +4,13 @@ from django.db import models
 
 
 class classroom(models.Model):
-	name = models.CharField(max_length=40)
+	name = models.CharField(max_length=200)
 	floor = models.CharField(max_length=20)
 	has_podium = models.CharField(max_length=20)
 	commissioned_date = models.DateTimeField('date commissioned')
-	created_at = models.TimeField(_(u"Created Time"), auto_now_add=True, blank=True)
-	updated_at = models.TimeField(_(u"Updated Time"), auto_now_add=True, blank=True)
+	created_at = models.TimeField(auto_now=False, auto_now_add=True)
+	updated_at = models.TimeField(auto_now_add=True, blank=False)
+
+
+	def __str__():
+		return slf.full_name

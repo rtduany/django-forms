659d609d4e5cba2bebe204c6db566f216e487a7d
rtduany <rtduany@gmail.com>
2015-10-08 22:53:59 +0300

    added first django project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ruot/Documents/projects/django-projects/django-forms/django-forms/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

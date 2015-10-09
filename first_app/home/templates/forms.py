8b31bb04569ef600ed9f2a854dae4832dcbdb3cd
rtduany <rtduany@gmail.com>
2015-10-10 01:03:45 +0300

    added code for forms.py file

---
+++ first_app/home/templates/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Student
+
+
+class StudentForm(forms.ModelForm):
+	class Meta:
+		# fields = ['full_name', 'email', 'age']
+		exclude = ['last_update']
+		model = Student
+
+	def clean_age(self):
+		age = self.cleaned_data.get('age')
+		if age > 120:
+			raise forms.validationError('you may be too old for this class')
+		elif age < 10:
+			raise forms.validationError('you may be young old for this class')
+		return age

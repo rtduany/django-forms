971b4ee1ba38b0faa747fb68cce2e6bff5b0ad36
rtduany <rtduany@gmail.com>
2015-10-09 13:14:44 +0300

    added modifications to index.html and admin.py to change to student

--- first_app/home/admin.py
+++ first_app/home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import classroom
+from .models import student
 # Register your models here.
 
 
-admin.site.register(classroom)
+admin.site.register(student)

99467c916836f6478f9594d5bbb5fe6de4581d9b
rtduany <rtduany@gmail.com>
2015-10-09 13:02:58 +0300

    added code to modify fields in the models.py file

--- first_app/home/models.py
+++ first_app/home/models.py
@@ -3,14 +3,14 @@ from django.db import models
 # Create your models here.
 
 
-class classroom(models.Model):
-	name = models.CharField(max_length=200)
-	floor = models.CharField(max_length=20)
-	has_podium = models.CharField(max_length=20)
-	commissioned_date = models.DateTimeField('date commissioned')
-	created_at = models.TimeField(auto_now=False, auto_now_add=True)
-	updated_at = models.TimeField(auto_now_add=True, blank=False)
+class student(models.Model):
+	full_name = models.CharField(max_length=200)
+	age = models.IntegerField()
+	email = models.EmailField()
+	interests = models.TextFields()
+	registered_date = models.DateField(auto_now=False, auto_now_add=True)
+	last_update = models.DateField(auto_now_add=True, blank=False)
 
 
 	def __str__():
-		return slf.full_name
+		return self.full_name

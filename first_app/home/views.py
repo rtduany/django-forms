25bfa5051fe686ad7dfd7cec062643c36d0373a3
rtduany <rtduany@gmail.com>
2015-10-10 01:15:15 +0300

    added code to views.py

--- first_app/home/views.py
+++ first_app/home/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+from .forms import StudentForm
 
 # Create your views here.
 
 
 def index(request):
+	form = StudentForm(request.POST or None)
+	if form.is_Valid():
+		form.save()
 	context = {
 		"hello_message": "hello Moringa"
+		"form": form
 	}
 	return render(request, 'index.html' , context)

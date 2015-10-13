10ae7ae09a3fd5d90f42da1b0c99b6a05742c464
rtduany <rtduany@gmail.com>
2015-10-13 10:21:44 +0300

    fix bug in admin.py file

--- first_app/home/admin.py
+++ first_app/home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import student
+from .models import Student
 # Register your models here.
 
 
-admin.site.register(student)
+admin.site.register(Student)

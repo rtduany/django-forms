948c084aa6dcac9ea2e746677a92239066c0e659
rtduany <rtduany@gmail.com>
2015-10-14 10:53:32 +0300

    add a fix to the search_term in views.py file

--- first_app/home/views.py
+++ first_app/home/views.py
@@ -44,7 +44,7 @@ def feedback(request):
 		return render(request, 'feedback.html' , context)
 
 def students(request):
-	search_term = request.GET['search']
+	search_term = request.GET.get('search', default='')
 	students = Student.objects.all().order_by('-last_update').filter(full_name__contains=search_term)
 	context = {'students': students}
 
